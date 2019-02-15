3f84ff9722b8976cb280999e697be48850dc3dc9
Aisa <aisajemila@gmail.com>
2019-02-15 17:18:27 +0300

    Include the innerHTML for display into html elements and getElementById for selection elements from html using id's

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,17 @@ function triangleCheck() {
   var b = parseInt(document.getElementById("sideB").value);
   var c = parseInt(document.getElementById("sideC").value);
 
-
-  if (a===0 || b===0 || c===0){
+  if(a===0 || b===0 || c===0) {
     document.getElementById("show").innerHTML = "Enter a valid value!";
     alert("Value entered must be greater than zero");
     confirm("Please try again Or see instructions.");
   }else if(a+b>c && b+c>a && c+a>b){
     if(a===b && b===c){
-      alert("This is an Equilateral Triangle");
-    }else if (a===b || b===c || c===a) {
-      alert("This is an Isosceles Triangle");
+      document.getElementById("show").innerHTML = "This is an Equilateral Triangle";
+    }else if (a===b || b===c || a===c){
+      document.getElementById("show").innerHTML = "This is an Isosceles Triangle";
     }else if(a!==b && b!==c && c!==b){
-      alert("This is a Scalene Triangle");
+      document.getElementById("show").innerHTML = "This is a Scalene Triangle";
     }
   }else{
     alert("Not a Triangle!");
