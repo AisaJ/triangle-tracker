d9a052233026bac9e53776aa4bc7d132c4c0dd21
Aisa <aisajemila@gmail.com>
2019-02-15 20:11:35 +0300

    Put the variables inside an array to get them from array.

--- js/scripts.js
+++ js/scripts.js
@@ -4,17 +4,19 @@ function triangleCheck() {
   var b = parseInt(document.getElementById("sideB").value);
   var c = parseInt(document.getElementById("sideC").value);
 
+  var dimension = [a,b,c];
+
   //checks if a dimension entered is 0
-  if(a===0 || b===0 || c===0) {
+  if(dimension[0]===0 || dimension[1]===0 || dimension[2]===0) {
     document.getElementById("show").innerHTML = "Enter a valid number!";
     alert("Value entered must be greater than zero");
     confirm("Please try again Or see instructions.");
-  }else if(a+b>c && b+c>a && c+a>b){
-    if(a===b && b===c){
+  }else if(a+dimension[1]>dimension[2] && dimension[1]+dimension[2]>dimension[0] && dimension[2]+dimension[0]>dimension[1]){
+    if(dimension[0]===dimension[1] && dimension[1]===dimension[2]){
       document.getElementById("show").innerHTML = "This is an Equilateral Triangle";
-    }else if (a===b || b===c || a===c){
+    }else if (dimension[0]===dimension[1] || dimension[1]===dimension[2] || dimension[0]===dimension[2]){
       document.getElementById("show").innerHTML = "This is an Isosceles Triangle";
-    }else if(a!==b && b!==c && c!==b){
+    }else if(dimension[0]!==dimension[1] && dimension[1]!==dimension[2] && dimension[2]!==dimension[1]){
       document.getElementById("show").innerHTML = "This is a Scalene Triangle";
     }
   }else{
