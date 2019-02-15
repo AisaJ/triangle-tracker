8070f0c6b2aca7877ef75de46172d0f09651baf6
Aisa <aisajemila@gmail.com>
2019-02-15 14:54:03 +0300

    Added a submit button for executing the function test(),which tests if input is 0.

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,7 @@ function test(){
 
   var [a,b,c] = sides;
   var show = document.getElementById("show");
-  if ()
+  if (a.length===0 && b.length ===0 c.length===0){
+    show = inner.HTML= "Please entr dimensions!";
+  }
 }
