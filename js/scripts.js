763e7e8c7b32307c96be22b7b9030c28b831f2db
Aisa <aisajemila@gmail.com>
2019-02-15 14:40:14 +0300

    Added a script of taking input values from a user and input the data into an array.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function test(){
+  var sides =[];
+
+  sides.push(document.getElementById("sideA").value);
+  sides.push(document.getElementById("sideB").value);
+  sides.push(document.getElementById("sideC").value);
+
+  var [a,b,c] = sides;
+  var show = document.getElementById("show");
+  if ()
+}
