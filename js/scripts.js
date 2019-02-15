2f221a8f5ba07e8ba260fc6e7818700e00f190bf
Aisa <aisajemila@gmail.com>
2019-02-15 16:47:56 +0300

    correct the declaration of variables and the condition.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,20 @@
-function test(){
-  var sides =[];
+function triangleCheck() {
 
-  sides.push(document.getElementById("sideA").value);
-  sides.push(document.getElementById("sideB").value);
-  sides.push(document.getElementById("sideC").value);
+  var a = parseInt(document.getElementById("sideA").value);
+  var b = parseInt(document.getElementById("sideB").value);
+  var c = parseInt(document.getElementById("sideC").value);
 
-  var [a,b,c] = sides.parseInt();
 
-  if (a.length===0 && b.length ===0 c.length===0){
+  if (a===0 || b===0 || c===0){
     document.getElementById("show").innerHTML = "Enter a valid value!";
     alert("Value entered must be greater than zero");
     confirm("Please try again Or see instructions.");
-  }else{
-    sides(a,b,c);
-  }
-}
-function sides(a,b,c){
-  if(a+b>c && b+c>a && c+a>b){
+  }else if(a+b>c && b+c>a && c+a>b){
     if(a===b && b===c){
       alert("This is an Equilateral Triangle");
     }else if (a===b || b===c || c===a) {
       alert("This is an Isosceles Triangle");
-    }else (a!==b && b!==c && c!==b){
+    }else if(a!==b && b!==c && c!==b){
       alert("This is a Scalene Triangle");
     }
   }else{
