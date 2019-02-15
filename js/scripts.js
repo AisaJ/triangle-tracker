b7a35903121f66a82ea58cf32a88fd0f1cffbaca
Aisa <aisajemila@gmail.com>
2019-02-15 15:47:17 +0300

    Added an if condition script to test for the types of triangles.

--- js/scripts.js
+++ js/scripts.js
@@ -5,9 +5,26 @@ function test(){
   sides.push(document.getElementById("sideB").value);
   sides.push(document.getElementById("sideC").value);
 
-  var [a,b,c] = sides;
-  var show = document.getElementById("show");
+  var [a,b,c] = sides.parseInt();
+
   if (a.length===0 && b.length ===0 c.length===0){
-    show = inner.HTML= "Please entr dimensions!";
+    document.getElementById("show").innerHTML = "Enter a valid value!";
+    alert("Value entered must be greater than zero");
+    confirm("Please try again Or see instructions.");
+  }else{
+    sides(a,b,c);
+  }
+}
+function sides(a,b,c){
+  if(a+b>c && b+c>a && c+a>b){
+    if(a===b && b===c){
+      alert("This is an Equilateral Triangle");
+    }else if (a===b || b===c || c===a) {
+      alert("This is an Isosceles Triangle");
+    }else (a!==b && b!==c && c!==b){
+      alert("This is a Scalene Triangle");
+    }
+  }else{
+    alert("Not a Triangle!");
   }
 }
