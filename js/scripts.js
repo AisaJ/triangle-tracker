4225932fa58dea266dfb21001e155f1f9957018d
Aisa <aisajemila@gmail.com>
2019-02-16 12:43:16 +0300

    Added an onload attribute to the about page body element. This is to display the names assigned from the script using triNames funtion.

--- js/scripts.js
+++ js/scripts.js
@@ -22,4 +22,15 @@ function triangleCheck() {
   }else{
     alert("Not a Triangle!");
   }
+
+}
+//Giving triangle names and displaying in the html document
+function triNames() {
+  var names = [];
+  names.push("Equilateral","Isosceles","Scalene");
+  var i = names[0];
+  var j = names[1];
+  var k = names[2];
+  document.getElementById("i").innerHTML = i;
+
 }
